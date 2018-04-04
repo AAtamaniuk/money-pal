9be63eebda0665f9fbe10109ca272caee4d0f59b
Oleksandr Atamaniuk <atol.if87@gmail.com>
2018-04-04 15:34:11 +0300

    Add test data

---
+++ src/testData/money.js
@@ -0,0 +1,40 @@
+const moneyData = [
+  {
+    id: '1',
+    type: 'income',
+    name: 'salary',
+    date: '12345',
+    value: 8000,
+  },
+  {
+    id: '2',
+    type: 'entertainment',
+    name: 'Go to the movie',
+    date: '12345',
+    value: 200,
+  },
+  {
+    id: '3',
+    type: 'food',
+    name: 'Go to the restaurant',
+    date: '12345',
+    value: 300,
+  },
+  {
+    id: '4',
+    type: 'shopping',
+    name: 'Buy pants',
+    date: '12345',
+    value: 40,
+  },
+  {
+    id: '5',
+    type: 'others',
+    name: 'Pay online',
+    date: '12345',
+    value: 100,
+  },
+];
+
+export default moneyData;
+
