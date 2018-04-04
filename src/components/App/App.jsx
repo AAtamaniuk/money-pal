19eddee93444b9f3c61648511b0bd885eaefa0b6
Oleksandr Atamaniuk <atol.if87@gmail.com>
2018-04-04 17:19:11 +0300

    Add AppBar and some styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 // Material UI
+import { withStyles } from 'material-ui/styles';
 import Typography from 'material-ui/Typography';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Paper from 'material-ui/Paper';
+import CssBaseline from 'material-ui/CssBaseline';
 // Styles
 import './App.css';
 // Test data
@@ -9,14 +14,25 @@ import moneyData from '../../testData/money';
 import Total from '../Total/Total';
 import MoneyList from '../MoneyList/MoneyList';
 
+const styles = () => ({
+  root: {
+
+  },
+  container: {
+    marginTop: 32,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '100%',
+    maxWidth: 1024,
+  },
+});
+
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      money: [
-
-      ],
+      money: [],
     };
     this.handleAddIncome = this.handleAddIncome.bind(this);
     this.handleAddCost = this.handleAddCost.bind(this);
@@ -38,17 +54,34 @@ class App extends Component {
 
   render() {
     const { money } = this.state;
+    const { classes } = this.props;
     const total = money
       .map(i => i.value)
       .reduce((sum, current) => sum + current, 0);
     return (
-      <div>
-        <Typography variant="headline">Money Pal</Typography>
-        <Total total={total} onAddIncome={this.handleAddIncome} onAddCost={this.handleAddCost} />
-        <MoneyList money={money} />
-      </div>
+      <React.Fragment>
+        <CssBaseline />
+        <div className={classes.root}>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="title" color="inherit">
+                Money Pal
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Paper className={classes.container}>
+            <Total
+              total={total}
+              onAddIncome={this.handleAddIncome}
+              onAddCost={this.handleAddCost}
+            />
+            <MoneyList money={money} />
+          </Paper>
+        </div>
+      </React.Fragment>
+
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
