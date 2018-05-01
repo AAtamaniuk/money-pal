fa125cda556a97e69b772d264a387f6e826bb40e
Oleksandr Atamaniuk <atol.if87@gmail.com>
2018-05-01 15:20:06 +0300

    Rewrite redux stoe file structure

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 // Material UI
 import { MuiThemeProvider } from 'material-ui/styles';
 // Configs
 import theme from '../../config/theme';
 // Redux
-import rootReducer from '../../reducers';
+import store from '../../store';
 // Components
 import App from '../App/App';
-
-const store = createStore(rootReducer);
 
 function Root() {
   return (
