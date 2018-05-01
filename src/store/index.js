cfde7d3d6a4d3ee9d29615d45634e223dbb7ef5c
Oleksandr Atamaniuk <atol.if87@gmail.com>
2018-05-01 16:09:12 +0300

    Add redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import rootReducer from '../reducers';
 
-const store = createStore(rootReducer);
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // eslint-disable
+
+const store = createStore(rootReducer, composeEnhancers());
+
 
 export default store;
